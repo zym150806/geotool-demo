ca91fd7bdc90852a8960bc8a6a7f4f2f7ca2c19a
zym <2218497554@qq.com>
2019-07-30 08:59:01 +0800

    do modify

--- src/main/java/com/neo/service/GeoService.java
+++ src/main/java/com/neo/service/GeoService.java
@@ -1,9 +1,21 @@
 package com.neo.service;
 
+import io.grpc.ServerServiceDefinition;
 import io.grpc.geoservice.GeoGrpc;
+import io.grpc.geoservice.InPolygonReply;
+import io.grpc.geoservice.InPolygonRequest;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+import java.util.List;
+
 @GrpcService
-public class GeoService implements GeoGrpc {
+public class GeoService extends GeoGrpc.GeoImplBase {
+    @Override
+    public void inPolygon(InPolygonRequest request, StreamObserver<InPolygonReply> responseObserver) {
+        List<InPolygonRequest.Point> polygonList = request.getPolygonList();
+
+
+    }
 
 }
